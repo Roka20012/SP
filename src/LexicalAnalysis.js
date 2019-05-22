982ae2b1328ed3c5bfcea47fcac19340df64184d
Roka20012 <broslavsky.rostislaw@gmail.com>
2019-05-23 00:37:25 +0300

    lexical analysis 1 part done

--- src/LexicalAnalysis.js
+++ src/LexicalAnalysis.js
@@ -1,31 +1,29 @@
+import commands from './tokens';
+
 class LexicalAnalysis {
-    checkConstants(element, index) {
-        let aboutElement = "";
-
-        if (element.match(/\d[a-h0-9]*h/gi)) {
-            //16 constants
-            aboutElement = `${index}. ${element} -> 16 константа\n`;
-        }
-        if (element.match(/^\d+$/)) {
-            //10 constants
-            aboutElement = `${index}. ${element} -> 10 константа\n`;
-        }
-        if (element.match(/[01]+b/gi)) {
-            //2 constants
-            aboutElement = `${index}. ${element} -> 2 константа\n`;
-        }
-
-        return aboutElement;
+    checkBinaryConst(el) {
+        return el.match(/[01]+b/gi) ? "2 константа" : "";
     }
 
-    checkId(element, index) {
-        let aboutElement = "";
+    checkDecimalConst(el) {
+        return el.match(/^\d+$/) ? "10 константа" : "";
+    }
 
-        if (element.match(/^([a-z?@$_&])([a-z?@$_&\d]*)$/gi)) {
-            aboutElement = `${index}. ${element} ->  Ідентифікатор користувача або нe визначений\n`;
+    checkHexConst(el) {
+        return el.match(/\d[a-h0-9]*h/gi) ? "16 константа" : "";
+    }
+
+    //indentificator
+    checkId(el) {
+        return el.match(/^([a-z?@$_&])([a-z?@$_&\d]*)$/gi) ? "Індентифікатор користувача або невизначений" : "";
+    }
+
+    checkCommand(el) {
+        if(Array.isArray(commands[el.toUpperCase()])) {
+            return commands[el.toUpperCase()][1];
         }
-
-        return aboutElement;
+    
+        return commands[el.toUpperCase()] ? commands[el.toUpperCase()] : "";
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import commands from "./tokens";
-import checkSentStructure from "./chapter2";
+// import checkSentStructure from "./chapter2";
 import LexicalAnalysis from "./LexicalAnalysis";
 
 let lexAnls = new LexicalAnalysis();
@@ -14,7 +13,13 @@ const createFile = (text, name, type) => {
     dlbtn.download = name;
 };
 
-const searchCommands = (commands, code) => {
+const searchCommands = code => {
+    for (let i = 0; i < code.length; i++) {
+        if (~code[i].indexOf(";")) {
+            code[i] = code[i].slice(0, code[i].indexOf(";"));
+        }
+    }
+
     code = code.map(el => el.match(/\.?(\w+|\W)/gi));
     for (let i = 0; i < code.length; i++) {
         if (code[i] === null) {
@@ -23,25 +28,21 @@ const searchCommands = (commands, code) => {
         }
     }
 
-    if (code[code.length - 1] === null) {
-        code.splice(code.length - 1, 1);
-        code.push([""]);
-    }
-
     let details = [];
-    console.log(code);
+
     code.forEach((el, i) => {
         details[i] = `Рядок: ${el.join("")}\n`;
         let j = 1;
         el.forEach(e => {
-            if (commands[e.toUpperCase()]) {
-                details[i] += `${j}. ${e} -> ${commands[e.toUpperCase()]}\n`;
-                j++;
-            } else if (lexAnls.checkId(e, j)) {
-                details[i] += lexAnls.checkId(e, j);
-                j++;
-            } else if (lexAnls.checkConstants(e, j)) {
-                details[i] += lexAnls.checkConstants(e, j);
+            let lexema =
+                lexAnls.checkCommand(e) ||
+                lexAnls.checkId(e) ||
+                lexAnls.checkBinaryConst(e) ||
+                lexAnls.checkDecimalConst(e) ||
+                lexAnls.checkHexConst(e);
+
+            if (!!lexema) {
+                details[i] += `${j}. ${e} -> ${lexema}\n`;
                 j++;
             }
         });
@@ -49,29 +50,24 @@ const searchCommands = (commands, code) => {
 
     details = details.join("\n");
     textareaElement2.innerHTML = details;
-    console.log("details");
-    console.log(details);
-    console.log("code");
-    console.log(code);
 
-    for (let i = 0; i < code.length; i++) {
-        for (let j = 0; j < code[i].length; j++) {
-            if (!code[i][j] || code[i][j].match(/\s+/gi)) {
-                console.log(code[i][j]);
-                code[i].splice(j, 1);
-                j--;
-            }
-        }
-    }
+    // for (let i = 0; i < code.length; i++) {
+    //     for (let j = 0; j < code[i].length; j++) {
+    //         if (!code[i][j] || code[i][j].match(/\s+/gi)) {
+    //             console.log(code[i][j]);
+    //             code[i].splice(j, 1);
+    //             j--;
+    //         }
+    //     }
+    // }
 
-    console.log(code);
+    // console.log(code);
 
     createFile(details, "myfile.txt", "text/plain");
 };
 
 inputElement.onchange = event => {
     let asmFile = event.target.files[0];
-    // console.log(asmFile.name.split(".")[1]);
     if (asmFile === undefined) return;
 
     if (asmFile.name.split(".")[1] === "asm" || asmFile.type === "text/plain") {
@@ -81,7 +77,7 @@ inputElement.onchange = event => {
             let text = event.target.result;
             textareaElement.value = text;
             let code = text.toString().split("\n");
-            searchCommands(commands, code);
+            searchCommands(code);
         };
 
         reader.onerror = _ => alert("Помилка читання файлу!");
